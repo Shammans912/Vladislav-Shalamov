cfa0ed9f80cb2b1e40bbcd593f4bde1ee91da32e
Shammans912 <62794308+Shammans912@users.noreply.github.com>
2020-03-28 14:01:19 +0300

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,51 @@
+# 1 урок 1 программа
+
+#num = int(input('Введие число: '))
+#num += 2
+#print(num)
+
+# 1 урок 2 программа
+
+#while True:
+    #num = int(input('Введие число: '))
+    #if 0 < num < 10:
+        #num = num ** 2
+        #print(num)
+        #break
+    #else:
+        #print('Введите число от 0 до 10')
+
+# 1 урок 3 программа
+
+#name = input('Введите имя: ')
+#surname = input('Ведите фамилию: ')
+#old = int(input('Введите возраст: '))
+#weight = int(input('Введите вес: '))
+#if old < 30 and 50 <= weight <= 120:
+    #print(name, surname, old, 'лет, вес', weight, '- хорошее состояние')
+#elif 30 < old <= 40 and (50 > weight or weight > 120):
+    #print(name, surname, old, 'лет, вес', weight, '- следует занятся собой')
+#elif 40 < old and (50 > weight or weight > 120):
+    #print(name, surname, old, 'лет, вес', weight, '- следует обратится к врачу!')
+
+#name=input('Ваше имя: ')
+#surname=input('Ваша фамилия: ')
+#age=int(input('Ваш возраст: '))
+#weight=int(input('Ваш вес: '))
+#if age<=30:
+    #if 50<weight<120:
+        #print('Вы в отличном состоянии!')
+#elif weight<50:
+        #print('Займитесь собой!')
+#else:
+        #print('Обратитесь к врачу!')
+#if 30<age<=40:
+    #if 120<weight or weight<50:
+        #print('Займитесь собой!')
+#else:
+        #print('Вы в отличном состоянии!')
+#if 40<age:
+    #if 120<weight or weight<50:
+        #print('Обратитесь к врачу!')
+#else:
+        #print('Вы в отличном состоянии!')
